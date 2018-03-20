c640fa0cae224a6e2f559e33839d397f61a2666b
Kyle White <kwdwhite@yahoo.com>
2018-03-20 14:45:07 -0400

    second

--- anagrams.js
+++ anagrams.js
@@ -1,30 +1,50 @@
 
 
-var anagramObject = {
-
-
-};
-
+let anagramObject = {};
+const spot = document.getElementById("wordsDiv");
 
 function alphabetize(a) {
     return a.toLowerCase().split("").sort().join("").trim();
 }
 
+function reset() {
+    anagramObject = {};
+    while (spot.firstChild) {
+        spot.removeChild(spot.firstChild);
+    }
+}
+
+function findAnagrams() {
+    if (Object.keys(anagramObject).length) {
+        reset();
+    }
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        const alphabetizedWord = alphabetize(word);
+
+        if (!anagramObject[alphabetizedWord]) {
+            anagramObject[alphabetizedWord] = []
+        }
+        anagramObject[alphabetizedWord].push(word);
+    }
+    return anagramObject;
+}
 
 
-document.getElementById("findButton").onclick = function (){
-    var typedText = document.getElementById("input").value;
-    var spot = document.getElementById("wordsDiv");
+document.getElementById("findButton").onclick = function () {
+    const anagramPossibilities = Object.keys(findAnagrams());
 
-    for (i = 0; i < words.length; i ++){
-        var store = words[i];
-        if(alphabetize(typedText) == alphabetize(store)){
-            console.log(store)
+    for (let i = 0; i < anagramPossibilities.length; i++) {
+        const alphabetizedWord = anagramPossibilities[i];
+        const anagramWords = anagramObject[alphabetizedWord];
 
-            // var span = document.createElement("span");
-            // var textContent = document.createTextNode(store + " ");
-            // span.appendChild(textContent);
-            // spot.appendChild(span);
+        if (anagramWords.length >= 5) {
+            const div = document.createElement("div");
+            const textContent = document.createTextNode(anagramWords);
+            div.appendChild(textContent);
+            spot.appendChild(div);
         }
     }
 }
+
